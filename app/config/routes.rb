2a0e2e92ae41e5c41c63299bebbd21bbbb73f8e2
Nebras Alnemer <alnemer.3@buckeyemail.osu.edu>
2015-04-15 20:49:46 -0400

    generate student model and controller

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'student/index'
+
+  get 'student/create'
+
+  get 'student/new'
+
+  get 'student/edit'
+
+  get 'student/update'
+
+  get 'student/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ app/db/migrate/20150416004831_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :major
+      t.string :rank
+
+      t.timestamps
+    end
+  end
+end
