07db06c72e6930af27f57b4504fd21379a9bd244
Lordkarl1993 <kjjahlqvist@gmail.com>
2015-04-16 22:28:27 -0400

    updating things for adding the login function

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417004517) do
+ActiveRecord::Schema.define(version: 20150417021002) do
 
   create_table "free_times", force: true do |t|
     t.integer  "student_id"
@@ -42,5 +42,14 @@ ActiveRecord::Schema.define(version: 20150417004517) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
