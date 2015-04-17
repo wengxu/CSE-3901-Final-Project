6ff49988439c4a52d42f8dce41c1c4e99b8e390f
Lordkarl1993 <kjjahlqvist@gmail.com>
2015-04-16 23:27:55 -0400

    more updates for login

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417021002) do
+ActiveRecord::Schema.define(version: 20150417024422) do
 
   create_table "free_times", force: true do |t|
     t.integer  "student_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150417021002) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
