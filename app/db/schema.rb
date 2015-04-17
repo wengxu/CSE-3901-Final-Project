6a7e23d6d50a68038688f3f0b433512b57d8a7d4
jayjayb <briska.1@osu.edu>
2015-04-16 20:10:43 -0400

    updated models and migrations

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416004831) do
+ActiveRecord::Schema.define(version: 20150416235008) do
 
   create_table "group", force: true do |t|
     t.string   "name"
     t.string   "members"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "groups", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
