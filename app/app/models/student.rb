f49b26e2cf9049153951caa9394521b3a48765ee
Xu <weng.88@buckeyemail.osu.edu>
2015-04-17 17:23:59 -0400

    add constrains on student and free time table

--- app/app/models/student.rb
+++ app/app/models/student.rb
@@ -1,3 +1,14 @@
 class Student < ActiveRecord::Base
 	belongs_to :groups
+  validates :name, :major, :rank, presence: true
+  validates_uniqueness_of :name, 
+    {message: 'the student name is already entered'}
+  validates :name, format: {
+    with: /\A[a-zA-Z]/,
+    message: 'must start with a character' 
+  }
+  validates :major, format: {
+    with: /\A[a-zA-Z]+\Z/,
+    message: 'must be a string' 
+  }
 end
