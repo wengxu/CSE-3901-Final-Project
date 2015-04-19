0bba81a401dd0d7d44a307017b3111d729a38c4d
Lordkarl1993 <kjjahlqvist@gmail.com>
2015-04-18 21:09:24 -0400

    login page added needs functionality still, in progress

--- app/app/controllers/sessions_controller.rb
+++ app/app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		#we log into the correct user and go to their profile
+  	else
+  		#we create a nice little error message
+  		flash[:danger] = 'Invalid email/password combination'
+  		render 'new'
+  	end
+  end
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'search/searchResult'
 
+  get 'signup'    => 'users#new'
+
   get 'login'     => 'sessions#new'
 
   post 'login'    => 'sessions#create'
