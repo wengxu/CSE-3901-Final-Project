b14e5bf28930fd37fa78eaa24a90724388804284
Nebras Alnemer <alnemer.3@buckeyemail.osu.edu>
2015-04-21 23:30:42 -0400

    fix search

--- app/app/models/group.rb
+++ app/app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ActiveRecord::Base
 	has_many :users
+
+	def self.search(query)
+	# where(:title, query) -> This would return an exact match of the query
+	where("name like ?", "%#{query}%")
+	end
+
 end
